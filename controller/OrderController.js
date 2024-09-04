3fbfd68cf36bbf6df10cf08cf3b78d48abcfa420
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-09-04 11:36:38 +0200

    Update

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -2,46 +2,46 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import { orders as cart } from '../model/index.js'
 
-const cartRouter = express.Router()
+const orderRouter = express.Router()
 
-cartRouter.use(bodyParser.json())
+orderRouter.use(bodyParser.json())
 
 
-cartRouter.get('/:userID', (req, res) => {
+orderRouter.get('/:userID', (req, res) => {
     cart.fetchCart(req.params.userID, res);
 });
 
-cartRouter.post('/addToCart', (req, res) => {
+orderRouter.post('/addToCart', (req, res) => {
     const { prodID, userID } = req.query;
     cart.addToCart(prodID, userID, res);
 });
 
-cartRouter.delete('/deleteFromCart', (req, res) => {
+orderRouter.delete('/deleteFromCart', (req, res) => {
     const { prodID, userID } = req.query;
     cart.deleteFromCart(prodID, userID, res);
 });
 
 
-// cartRouter.get('/', (req, res) => {
+// orderRouter.get('/', (req, res) => {
 //     cart. fetchCart(req, res)
 // })
 
 
-// cartRouter.get('/:id', (req, res) => {
+// orderRouter.get('/:id', (req, res) => {
 //     cart.itemsInCart(req, res)
 // })
 
-// cartRouter.post('/add', (req, res) => {
+// orderRouter.post('/add', (req, res) => {
 //     cart.addToCart(req, res)
 // })
 
-// cartRouter.delete('/:id', (req, res) => {
+// orderRouter.delete('/:id', (req, res) => {
 //     cart.deleteFromCart(req, res)
 // })
 
 
 export {
-    cartRouter
+    orderRouter
 }
 
 
